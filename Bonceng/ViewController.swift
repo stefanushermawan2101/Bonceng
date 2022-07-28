5cf101ff778a08546d8964be2080ad12b2738337
Stefanus Hermawan Sebastian <stefanushermawan2101@gmail.com>
2022-07-28 09:00:26 +0700

    Initial Commit

---
+++ Bonceng/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bonceng
+//
+//  Created by Stefanus Hermawan Sebastian on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
